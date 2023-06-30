22f758217983d556d6573e2a3aec887206711033
ldavies99 <Luke.Davies@ext.ons.gov.uk>
2023-06-30 16:33:45 +0100

    Standardised package with the python version and added test

--- R/af_colours.R
+++ R/af_colours.R
@@ -1,15 +1,15 @@
 #' Analysis Function colour palette function
 #'
-#' Generate a colour palette based on the selected chart and code type (hex/rgb/names).
+#' Generate a colour palette based on the selected chart and colour_format type (hex/rgb/names).
 #'
 #' @param type Name of required palette. Choices are:
 #'   \code{categorical}, \code{duo},  \code{sequential},
 #'   \code{focus}
-#' @param n Number of colours to return, required for categorical palette type.
-#'   If omitted from other palette types, uses all colours.
-#' @param code Type of colour code to return. Choices are:
+#' @param colour_format Type of colour code to return. Choices are:
 #'   \code{hex}, \code{rgb}, \code{names}
 #'   If omitted, returns hex code
+#' @param n Number of colours to return for categorical palette type (max 6).
+#'   If omitted, uses all colours.
 #' @return A vector of colour codes
 #' @import
 #'   dplyr
@@ -19,39 +19,48 @@
 #'            geom_point() +
 #'            scale_colour_manual(values = af_colours("duo"))
 
-af_colours <- function(type = c("categorical", "duo", "sequential", "focus"), n, code = "hex") {
+af_colours <- function(type = c("categorical", "duo", "sequential", "focus"), colour_format = "hex", n = 6) {
 
-  type <- match.arg(type)
+  tryCatch(
+    expr = {
+      type <- match.arg(type)
+    },
+    error = function(e){
+      stop(paste0(type, " is not an available palette option. Please select either 'categorical', 'duo', 'sequential', or 'focus'."))
+    }
+  )
 
-  palette <- palette_picker(type, tolower(code))
+  if (!(colour_format %in% c("hex", "rgb", "names"))){
+    stop(paste0(colour_format, " is not an available colour format option. Please select either 'hex', 'rgb', or 'names'."))
+  }
 
-  if (type == "categorical" && missing(n)){
-    stop("Number of colours (n) is required for categorical palette")
+
+  if (type == "categorical" && n > 6){
+    stop("The number of colours (n) must not be more than 6 for the categorical palette.")
   }
 
   if (type == "categorical" && n == 2){
-    palette <- palette_picker("duo", tolower(code))
+    type  <- "duo"
   }
 
-  if (missing(n) && code == "rgb"){
-    n <- nrow(palette)
-  }
-    else
-      if (missing(n)){
-    n <- length(palette)
-  }
+  palette <- palette_picker(type, tolower(colour_format))
 
   if (n > length(palette)){
-    stop("Not enough colours in this palette")
+    message("Warning: list of colours returned is shorter than number of colours requested. Consult guidance to ensure correct palette chosen.")
+  }
+
+  if (n < length(palette)){
+    message("Warning: list of colours returned is longer than number of colours requested. Consult guidance to ensure correct palette chosen.")
+  }
+
+  if (type != "categorical"){
+    n <- length(palette)
   }
 
   if (n > 2){
     message("Line charts using more than two colours may not be accessible to all users")
   }
 
-
   head(palette, n)
 
 }
-
-
--- tests/testthat/test-af_colours.R
+++ tests/testthat/test-af_colours.R
@@ -1,27 +1,22 @@
-test_that("af_colour function works with palette types", {
-  expect_equal(af_colours("categorical", 5), c("#12436D", "#28A197", "#801650", "#F46A25", "#3D3D3D"))
-  expect_equal(af_colours("duo"), c("#12436D", "#F46A25"))
-  expect_equal(af_colours("sequential"), c("#12436D", "#2073BC", "#6BACE6"))
-  expect_equal(af_colours("focus"), c("#12436D", "#BFBFBF"))
+test_that("user must choose an available palette option", {
+  expect_error(af_colours("test"), "test is not an available palette option. Please select either 'categorical', 'duo', 'sequential', or 'focus'.", fixed = TRUE)
 })
 
-test_that("user must specify number of colours for categorical palette", {
-  expect_error(af_colours("categorical"), "Number of colours (n) is required for categorical palette", fixed = TRUE)
+test_that("user must choose an available colour format option", {
+  expect_error(af_colours("categorical", colour_format="test"), "test is not an available colour format option. Please select either 'hex', 'rgb', or 'names'.", fixed = TRUE)
+})
+
+test_that("user must choose at maximum 6 colours for the categorical palette", {
+  expect_error(af_colours("categorical", n=7), "The number of colours (n) must not be more than 6 for the categorical palette.", fixed = TRUE)
 })
 
 test_that("n gives the correct number of colours", {
-  expect_equal(af_colours("categorical", 4), c("#12436D", "#28A197", "#801650", "#F46A25"))
-  expect_error(af_colours("sequential", 4), "Not enough colours in this palette", fixed = TRUE)
-})
-
-test_that("function is returning the correct code type", {
-  expect_equivalent(af_colours("duo", code = "rgb"), rbind(c(18, 67, 109), c(244, 106, 37)))
-  expect_equal(af_colours("focus", code = "names"), c("dark_blue", "grey"))
+  expect_equal(length(af_colours("categorical", n=4)), 4)
+  expect_equal(length(af_colours("sequential", n=3)), 3)
+  expect_equal(nrow(af_colours("duo", "rgb", n=2)), 2)
+  expect_equal(nrow(af_colours("focus", "rgb", n=2)), 2)
 })
 
 test_that("categorical type switches to duo if n = 2", {
-  expect_equal(af_colours("categorical", 2), c("#12436D", "#F46A25"))
+  expect_equal(af_colours("categorical", n=2), c("#12436D", "#F46A25"))
 })
-
-
-
