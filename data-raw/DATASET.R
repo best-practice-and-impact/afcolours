6d48eb53a23d5fe12530359a18228f478f6a70a1
Luke Davies <117165341+ldavies99@users.noreply.github.com>
2023-07-27 15:03:22 +0100

    Move colour palette hex codes into a .rda file (#7)
    * Move colour palette hex codes into a .rda file and remove redundant functions
    
    * Remove palette_picker function

---
+++ data-raw/DATASET.R
@@ -0,0 +1,10 @@
+## code to prepare colour palettes goes here
+
+palette_types <- lst(
+  categorical = c("#12436D", "#28A197", "#801650", "#F46A25", "#3D3D3D", "#A285D1"),
+  duo = c("#12436D", "#F46A25"),
+  focus = c("#12436D", "#BFBFBF"),
+  sequential = c("#12436D", "#2073BC", "#6BACE6")
+  )
+
+usethis::use_data(palette_types, internal = TRUE, overwrite = TRUE)
--- tests/testthat/test-af_colours.R
+++ tests/testthat/test-af_colours.R
@@ -14,7 +14,20 @@ test_that("user must choose at minimum 1 colour for the categorical palette", {
   expect_error(af_colours("categorical", n=0), "The number of colours (n) must be more than 0 for the categorical palette.", fixed = TRUE)
 })
 
-test_that("n gives the correct number of colours", {
+test_that("works with palette types", {
+  expect_equal(af_colours("categorical"), c("#12436D", "#28A197", "#801650", "#F46A25", "#3D3D3D", "#A285D1"))
+  expect_equal(af_colours("duo"), c("#12436D", "#F46A25"))
+  expect_equal(af_colours("sequential"), c("#12436D", "#2073BC", "#6BACE6"))
+  expect_equal(af_colours("focus"), c("#12436D", "#BFBFBF"))
+})
+
+test_that("returns the correct colour format", {
+  expect_equal(c(af_colours("duo", colour_format = "rgb")), c(18, 244, 67, 106, 109, 37))
+  expect_equal(af_colours("focus"), c("#12436D", "#BFBFBF"))
+})
+
+
+test_that("n returns the correct number of colours", {
   expect_equal(length(af_colours("categorical", n=4)), 4)
   expect_equal(length(af_colours("sequential", n=3)), 3)
   expect_equal(nrow(af_colours("duo", "rgb", n=2)), 2)
