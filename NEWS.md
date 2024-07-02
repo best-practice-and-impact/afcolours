655927a8a85aed17f03caff82c62235a75d805b1
Luke Davies <117165341+ldavies99@users.noreply.github.com>
2024-07-02 16:42:51 +0100

    Add verbose parameter and style package (#13)
    * Add parameter to prevent afcolours returning messages and style package with styler
    
    * Add changes to NEWS.md

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # afcolours 1.0.0
 
+* `af_colours()` now allows users to prevent messages being returned to the console.
+
 # afcolours 0.0.0.9000
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/af_colours.R
+++ R/af_colours.R
@@ -10,62 +10,59 @@
 #'   If omitted, returns hex code
 #' @param n Number of colours to return for categorical palette type (max 6).
 #'   If omitted, uses all colours.
+#' @param verbose Option to stop function producing messages to the console.
 #' @returns A vector of colour codes
 #' @import
 #'   dplyr
 #' @export
 #' @keywords colours
 #' @examples
-#' data <- data.frame(x = c(1,2),
-#'                    y = c(1,2),
-#'                    z = c("a","b")
-#'         )
+#' data <- data.frame(
+#'   x = c(1, 2),
+#'   y = c(1, 2),
+#'   z = c("a", "b")
+#' )
 #' ggplot2::ggplot(data, ggplot2::aes(x = x, y = y, colour = z)) +
-#' ggplot2::geom_point() +
-#' ggplot2::scale_colour_manual(values = af_colours("duo"))
-
-af_colours <- function(type = c("categorical", "duo", "sequential", "focus"), colour_format = "hex", n = 6) {
-
+#'   ggplot2::geom_point() +
+#'   ggplot2::scale_colour_manual(values = af_colours("duo"))
+af_colours <- function(type = c("categorical", "duo", "sequential", "focus"),
+                       colour_format = "hex",
+                       n = 6,
+                       verbose = TRUE) {
   tryCatch(
     expr = {
       type <- match.arg(type)
     },
-    error = function(e){
+    error = function(e) {
       stop(paste0(type, " is not an available palette option. Please select either 'categorical', 'duo', 'sequential', or 'focus'."))
     }
   )
 
-  if (!(colour_format %in% c("hex", "rgb"))){
+  if (!(colour_format %in% c("hex", "rgb"))) {
     stop(paste0(colour_format, " is not an available colour format option. Please select either 'hex' or 'rgb'."))
   }
 
 
-  if (type == "categorical"){
-
-    if (n > 6){
+  if (type == "categorical") {
+    if (n > 6) {
       stop("The number of colours (n) must not be more than 6 for the categorical palette.")
     }
 
-    if (n <= 0){
+    if (n <= 0) {
       stop("The number of colours (n) must be more than 0 for the categorical palette.")
     }
 
-    if (n > 4){
+    if (n > 4 & verbose) {
       message("It is best practice to limit to four categories when using the categorical palette so the chart does not become too cluttered.")
     }
 
-    if (n == 2){
-      type  <- "duo"
+    if (n == 2) {
+      type <- "duo"
     }
-
-  } else if (type == "sequential"){
-
+  } else if (type == "sequential" & verbose) {
     message("This palette should only be used for sequential data and please ensure you give all bars a dark blue outline if you are creating a bar chart.")
-
-  } else if (type == "focus"){
-
+  } else if (type == "focus" & verbose) {
     message("This palette should only be used to highlight specific elements to help users understand the information.")
-
   }
 
   palette <- palette_types[[type]]
@@ -76,13 +73,12 @@ af_colours <- function(type = c("categorical", "duo", "sequential", "focus"), co
     palette <- t(grDevices::col2rgb(palette))
   }
 
-  if (type != "categorical"){
-    if (n != 6){
+  if (type != "categorical") {
+    if (n != 6 & verbose) {
       message("Warning: list of colours returned may be different than the colours requested. Consult guidance to ensure correct palette chosen.")
     }
     n <- palette_length
   }
 
   utils::head(palette, n)
-
 }
